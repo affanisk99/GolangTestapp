3e4ebace44a022ba8d8ed31256848fffd9c773e0
affanisk99 <affan.iskandarsyah@gmail.com>
2023-06-29 15:01:31 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstName string = "Affan"
+
+	var lastName string
+	lastName = "Isk"
+	fmt.Printf("Halo %s %s Ganteng! \n", firstName, lastName)
+	// fmt.Println("Pasti Bisa Jago Golang, Bukan Jago Ngebolang di siang bolong doang")
+}
